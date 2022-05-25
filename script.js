6ab6106accdd85eb7af3b010ff68465dc384f11f
Luane Caroline <luanecrln@gmail.com>
2022-05-24 23:38:30 -0300

    project init

---
+++ script.js
@@ -0,0 +1,46 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        numeroHtml += '<div class = "numero"></div>'
+    }
+
+    seuVotoPara.style.display = "none"
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = "none";
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+    
+}
+
+function clicou(n) {
+    alert("clicou em" +n);
+}
+function branco() {
+    alert("Clicou em BRANCO");
+}
+function corrige() {
+    alert("Clicou em CORRIGE")
+}
+function confirma() {
+    alert("clicou em CONFIRMA")
+}
+
+
+comecarEtapa();
